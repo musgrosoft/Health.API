53301b0f05750b26882b103daec9562bd41e7baa
Tim <tim.musgrove@totaljobsgroup.com>
2018-03-12 16:26:38 +0000

    add moving average to resting heart rate

--- HealthAPI/Controllers/RestingHeartRatesController.cs
+++ HealthAPI/Controllers/RestingHeartRatesController.cs
@@ -17,11 +17,43 @@ namespace HealthAPI.Controllers
 
         // GET api/bloodpressures
         [HttpGet]
-        public IEnumerable<RestingHeartRate> Get()
+        public IEnumerable<ViewModels.RestingHeartRate> Get()
         {
-            return _context.RestingHeartRate.OrderBy(x=>x.DateTime);
+            var beats = _context.RestingHeartRate.Select(x=>new ViewModels.RestingHeartRate
+            {
+                DateTime = x.DateTime,
+                Beats = x.Beats
+                    
+            }).OrderBy(y=>y.DateTime).ToList();
+
+            AddMovingAverages(beats, 10);
+
+            return beats;
+
         }
 
-        
+        private void AddMovingAverages(List<ViewModels.RestingHeartRate> heartRates, int period)
+        {
+            for (int i = 0; i < heartRates.Count(); i++)
+            {
+                if (i >= period - 1)
+                {
+                    decimal total = 0;
+                    for (int x = i; x > (i - period); x--)
+                        total += heartRates[x].Beats;
+                    decimal average = total / period;
+                    // result.Add(series.Keys[i], average);
+                    heartRates[i].MovingAverageBeats = average;
+                }
+                else
+                {
+                    //heartRates[i].MovingAverageBeats = heartRates[i].Beats;
+                    heartRates[i].MovingAverageBeats = null;
+                }
+
+            }
+        }
+
+
     }
 }
