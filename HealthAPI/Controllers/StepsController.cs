969c5c37cb028cc3075add708199f13d133ce1d0
Tim <tim.musgrove@totaljobsgroup.com>
2018-03-13 15:28:40 +0000

    routing

--- HealthAPI/Controllers/StepsController.cs
+++ HealthAPI/Controllers/StepsController.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HealthAPI.Controllers
 {
-    [Route("api/[controller]")]
+  //  [Route("api/[controller]")]
     public class StepsController : Controller
     {
         private readonly HealthContext _context;
