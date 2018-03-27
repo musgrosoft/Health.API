8c34dc35e0aada425671d228f8fe9bb625ced7bc
Tim <t>
2018-03-27 22:25:47 +0100

    update steps

--- HealthAPI/Models/DailySteps.cs
+++ HealthAPI/Models/DailySteps.cs
@@ -5,7 +5,7 @@ namespace HealthAPI.Models
     public class DailySteps
     {
         public DateTime DateTime { get; set; }
-        public string DataSource { get; set; }
+        //public string DataSource { get; set; }
         public int? Steps { get; set; }
     }
 }
