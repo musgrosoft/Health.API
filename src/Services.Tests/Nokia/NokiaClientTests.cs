95252e30951e9e459d84ba53c07828a51ca6015d
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-05-11 13:54:52 +0100

    tests

--- src/Services.Tests/Nokia/NokiaClientTests.cs
+++ src/Services.Tests/Nokia/NokiaClientTests.cs
@@ -18,6 +18,7 @@ namespace Services.Tests.Nokia
         [Fact]
         public async Task ShouldGetScaleMeasures()
         {
+            Uri capturedUri = new Uri("http://www.test.com");
 
             var httpMessageHandler = new Mock<HttpMessageHandler>();
             httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
@@ -25,7 +26,7 @@ namespace Services.Tests.Nokia
                 {
                     StatusCode = HttpStatusCode.OK,
                     Content = new StringContent(nokiaContent)
-                }));
+                })).Callback<HttpRequestMessage, CancellationToken>((h, c) => capturedUri = h.RequestUri); ;
 
             var httpClient = new HttpClient(httpMessageHandler.Object);
 
@@ -33,6 +34,7 @@ namespace Services.Tests.Nokia
 
             var weights = await nokiaClient.GetScaleMeasures(new DateTime());
 
+            Assert.Equal("http://api.health.nokia.com/measure?action=getmeas&oauth_consumer_key=ebb1cbd42bb69687cb85ccb20919b0ff006208b79c387059123344b921837d8d&oauth_nonce=742bef6a3da52fbf004573d18b8f04cf&oauth_signature=cgO95H%2Fg2qx0VQ9ma2k8qeHronM%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1503326610&oauth_token=7f027003b78369d415bd0ee8e91fdd43408896616108b72b97fd7c153685f&oauth_version=1.0&userid=8792669", capturedUri.AbsoluteUri);
             Assert.Equal(2, weights.Count());
             Assert.Contains(weights, x => x.Kg == (Decimal)90.435 && x.DateTime == new DateTime(2018,5,11,4,57,27));
             Assert.Contains(weights, x => x.Kg == (Decimal)90.261 && x.DateTime == new DateTime(2018,5,10,5,4,42));
