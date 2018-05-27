0d380a9f7dad0e2dd4e7a3f952a4b91d97a72781
Tim <t>
2018-05-27 11:55:54 +0100

    alc targetb

--- src/Repositories/Models/AlcoholIntake.cs
+++ src/Repositories/Models/AlcoholIntake.cs
@@ -12,7 +12,7 @@ namespace Repositories.Models
         public int Units { get; set; }
         
         [NotMapped]
-        public double? Target
+        public double? TargetA
         {
             get
             {
@@ -21,6 +21,25 @@ namespace Repositories.Models
                 return days.TotalDays * 4;
             }
         }
+
+        [NotMapped]
+        public double? TargetB
+        {
+            //5086
+            //Mon May 21 2018
+            get
+            {
+                if (DateTime < new DateTime(2018, 5, 21))
+                {
+                    return null;
+                }
+
+                var days = DateTime - new DateTime(2018, 5, 20);
+
+                return 5086 + days.TotalDays * 3;
+            }
+        }
+
 
         public int? CumSumUnits { get; set; }
     }
