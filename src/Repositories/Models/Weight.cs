e80d68de0bd6fa35fd29e96db256eabdc0784660
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-05-31 11:44:13 +0100

    remove target from weight domain obj

--- src/Repositories/Models/Weight.cs
+++ src/Repositories/Models/Weight.cs
@@ -19,28 +19,6 @@ namespace Repositories.Models
         [Column(TypeName = "decimal(10, 5)")]
         public decimal? MovingAverageKg { get; set; }
 
-        [NotMapped]
-        public double? Target { get {
-
-                //if (DateTime < new DateTime(2018, 5, 21))
-                if (DateTime < new DateTime(2018, 5, 1))
-                {
-                    return null;
-                }
-                else
-                {
-                    //var daysDiff = (DateTime - new DateTime(2018, 5, 21)).TotalDays;
-                    //return 90.4 - (daysDiff * 0.017);
-
-                    var daysDiff = (DateTime - new DateTime(2018, 5, 1)).TotalDays;
-                    return 90.74 - (daysDiff * 0.017);
-
-                }
-                
-
-
-
-            } } 
         
     }
 }
