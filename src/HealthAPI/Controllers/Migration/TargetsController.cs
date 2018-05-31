49008a6c28335b433df9bdbc73c7c7a01c56eddb
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-05-31 11:34:57 +0100

    targets

---
+++ src/HealthAPI/Controllers/Migration/TargetsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.Models;
+
+namespace HealthAPI.Controllers.Migration
+{
+    public class TargetsController : Controller
+    {
+        // GET
+        //public IActionResult Index()
+        //{
+        //    return
+        //    View();
+        //}
+
+        [HttpGet]
+        public IActionResult Weights()
+        {
+            var targets = new List<Weight>();
+
+            var targetStartDate = new DateTime(2018, 5, 1);
+            var targetEndDate = DateTime.Now.AddDays(100);
+            var totalDays = (targetEndDate - targetStartDate).TotalDays;
+
+            var weightOnTargetStartDate = 90.74;
+            var targetDailyWeightLoss = 0.5 / 30;
+
+            for (var i = 0 ; i <= totalDays ; i++)
+            {
+                var target = new Weight
+                {
+                    DateTime = targetStartDate.AddDays(i),
+                    Kg = (Decimal)(weightOnTargetStartDate - (i * targetDailyWeightLoss))
+                };
+
+                targets.Add(target);
+            }
+
+            return Json(targets);
+        }
+    }
+}
