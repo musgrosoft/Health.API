f955f88b43c9252af90b52eba2a303805823a95b
Tim <t>
2018-06-05 21:21:17 +0100

    g

---
+++ src/Services/Google/GoogleAuthenticator.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json;
+using Services.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Google
+{
+    public class GoogleAuthenticator
+    {
+
+        private readonly OAuthService _oAuthService;
+        //        private readonly ILambdaLogger _logger;
+        private const string GOOGLE_SERVER = "https://www.googleapis.com";
+        
+        public GoogleAuthenticator(OAuthService oAuthService)
+        {
+            _oAuthService = oAuthService;
+        }
+
+        public async Task<string> GetAccessToken()
+        {
+
+            var refreshToken = await _oAuthService.GetGoogleRefreshToken();
+
+            var newTokens = await GetTokens(refreshToken);
+
+            var newAccessToken = newTokens.access_token;
+//            var newRefreshToken = newTokens.refresh_token;
+
+            await _oAuthService.SaveFitbitAccessToken(newAccessToken);
+  //          await _oAuthService.SaveFitbitRefreshToken(newRefreshToken);
+
+
+            return newAccessToken;
+        }
+
+        public async Task<GoogleRefreshTokenResponse> GetTokens(string refreshToken)
+        {
+            var client = new HttpClient();
+            var uri = GOOGLE_SERVER + "/oauth2/v4/token";
+            //client.DefaultRequestHeaders.Add("Authorization", "Basic " + "MjI4UFI4OjAyZjIyODBkOTY2MWQwMWFiNDlkY2Q1NWJhMjE4OTFh");
+         //   client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
+
+            var nvc = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("client_secret", "SHjTFZP4qSPL7aK-5Ctb1S0Q"),
+                new KeyValuePair<string, string>("grant_type", "refresh_token"),
+                new KeyValuePair<string, string>("refresh_token", refreshToken),
+                new KeyValuePair<string, string>("client_id", "407408718192.apps.googleusercontent.com"),
+            };
+
+            var response = await client.PostAsync(uri, new FormUrlEncodedContent(nvc));
+
+
+            //            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+
+
+            return JsonConvert.DeserializeObject<GoogleRefreshTokenResponse>(responseBody);
+        }
+
+
+
+
+    }
+}
--- src/Services/OAuth/OAuthService.cs
+++ src/Services/OAuth/OAuthService.cs
@@ -8,6 +8,9 @@ namespace Services.OAuth
         private const string FITBIT_ACCESS_TOKEN_KEY = "fitbit_access_token";
         private const string FITBIT_REFRESH_TOKEN_KEY = "fitbit_refresh_token";
 
+        private const string GOOGLE_ACCESS_TOKEN_KEY = "google_access_token";
+        private const string GOOGLE_REFRESH_TOKEN_KEY = "google_refresh_token";
+        
         private readonly IOAuthTokenRepository _oAuthTokenRepository;
         
         public OAuthService(IOAuthTokenRepository oAuthTokenRepository )
@@ -28,6 +31,21 @@ namespace Services.OAuth
         public async Task<string> GetFitbitRefreshToken()
         {
             return await _oAuthTokenRepository.ReadToken(FITBIT_REFRESH_TOKEN_KEY);
+        }
+
+        public async Task<string> GetGoogleRefreshToken()
+        {
+            return await _oAuthTokenRepository.ReadToken(GOOGLE_REFRESH_TOKEN_KEY);
+        }
+
+        public async Task SaveGoogleAccessToken(string token)
+        {
+            await _oAuthTokenRepository.SaveToken(GOOGLE_ACCESS_TOKEN_KEY, token);
+        }
+
+        public async Task SaveGoogleRefreshToken(string token)
+        {
+            await _oAuthTokenRepository.SaveToken(GOOGLE_REFRESH_TOKEN_KEY, token);
         }
 
         //public string GetFitbitAccessToken()
