af9e0bb907aef15c700dc634a546636c7c40a961
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-06-06 09:01:50 +0100

    g

---
+++ src/Migrators.Unit.Tests/GoogleMigratorTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Migrators.Unit.Tests
+{
+    public class GoogleMigratorTests
+    {
+        [Fact]
+        public void ShouldGetData()
+        {
+            var gMig = new GoogleMigrator();
+            var data = gMig.GetAlcoholIntakes(new DateTime(), new DateTime());
+
+            Assert.NotNull(data);
+        }
+    }
+}
