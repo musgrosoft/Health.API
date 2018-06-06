87bf83e6fa6ac367fdf55f5a22848c4de8a93cb2
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-06-06 16:41:38 +0100

    f

--- src/Services/Fitbit/Domain/FitbitDailyActivity.cs
+++ src/Services/Fitbit/Domain/FitbitDailyActivity.cs
@@ -4,21 +4,21 @@ using System.Collections.Generic;
 namespace Services.Fitbit.Domain
 {
 
-    public class Activity
-    {
-        public int activityId { get; set; }
-        public int activityParentId { get; set; }
-        public int calories { get; set; }
-        public string description { get; set; }
-        public double distance { get; set; }
-        public int duration { get; set; }
-        public bool hasStartTime { get; set; }
-        public bool isFavorite { get; set; }
-        public int logId { get; set; }
-        public string name { get; set; }
-        public string startTime { get; set; }
-        public int steps { get; set; }
-    }
+//    public class Activity
+//    {
+//        public int activityId { get; set; }
+//        public int activityParentId { get; set; }
+//        public int calories { get; set; }
+//        public string description { get; set; }
+//        public double distance { get; set; }
+//        public int duration { get; set; }
+//        public bool hasStartTime { get; set; }
+//        public bool isFavorite { get; set; }
+//        public int logId { get; set; }
+//        public string name { get; set; }
+//        public string startTime { get; set; }
+//        public int steps { get; set; }
+//    }
 
 //    public class Goals
 //    {
@@ -52,7 +52,7 @@ namespace Services.Fitbit.Domain
 
     public class FitbitDailyActivity
     {
-        public List<Activity> activities { get; set; }
+        //public List<Activity> activities { get; set; }
         //public Goals goals { get; set; }
         public Summary summary { get; set; }
         public DateTime DateTime { get; set; }
