3714ff41dccdb0668b7ed400e80a4817b7da0160
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-06-22 16:14:10 +0100

    t

---
+++ src/HealthAPI.Acceptance.Tests/Domain/StepCount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HealthAPI.Acceptance.Tests.Domain
+{
+    public class StepCount
+    {
+        public DateTime DateTime { get; set; }
+        public int? Count { get; set; }
+        public int? CumSumCount { get; set; }
+    }
+}
---
+++ src/HealthAPI.Acceptance.Tests/StepCountAcceptanceTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HealthAPI.Acceptance.Tests.Domain;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace HealthAPI.Acceptance.Tests
+{
+    public class StepCountAcceptanceTests
+    {
+        private HttpClient _httpClient;
+
+        public StepCountAcceptanceTests()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        [Fact]
+        public async Task ShouldGetWeights()
+        {
+            var uri = "http://musgrosoft-health-api.azurewebsites.net/odata/StepCounts";
+            _httpClient.DefaultRequestHeaders.Clear();
+
+            var response = await _httpClient.GetAsync(uri);
+            Assert.True(response.IsSuccessStatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<ODataResponse<StepCount>>(content);
+            Assert.NotNull(data);
+            Assert.True(data.value.Count > 860);
+
+            var lastStepCount = data.value.OrderByDescending(x => x.DateTime).FirstOrDefault();
+            Assert.True(lastStepCount.Count < 30000);
+            Assert.True(lastStepCount.Count > 10);
+        }
+    }
+}
