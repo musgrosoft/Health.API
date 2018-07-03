cba081b17d2aa072f5df65ea06fd04e430b56de9
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-07-03 09:32:12 +0100

    d

--- src/HealthAPI.Unit.Tests/Controllers/OData/WeightsControllerTests.cs
+++ src/HealthAPI.Unit.Tests/Controllers/OData/WeightsControllerTests.cs
@@ -1,49 +1,49 @@
-﻿using HealthAPI.Controllers.OData;
-using Repositories.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Xunit;
+﻿//using HealthAPI.Controllers.OData;
+//using Repositories.Models;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using Xunit;
 
-namespace HealthAPI.Unit.Tests.Controllers.OData
-{
-    public class WeightsControllerTests : IDisposable
-    {
-        private WeightsController _controller;
-        private FakeLocalContext _fakeLocalContext;
+//namespace HealthAPI.Unit.Tests.Controllers.OData
+//{
+//    public class WeightsControllerTests : IDisposable
+//    {
+//        private WeightsController _controller;
+//        private FakeLocalContext _fakeLocalContext;
 
-        public WeightsControllerTests()
-        {
-            _fakeLocalContext = new FakeLocalContext();
-            _controller = new WeightsController(_fakeLocalContext);
-        }
+//        public WeightsControllerTests()
+//        {
+//            _fakeLocalContext = new FakeLocalContext();
+//            _controller = new WeightsController(_fakeLocalContext);
+//        }
 
-        public void Dispose()
-        {
-            _fakeLocalContext.Database.EnsureDeleted();
-        }
+//        public void Dispose()
+//        {
+//            _fakeLocalContext.Database.EnsureDeleted();
+//        }
 
-        [Fact]
-        public void ShouldGetBloodPressures()
-        {
+//        [Fact]
+//        public void ShouldGetBloodPressures()
+//        {
 
-            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 1), Kg = 1 });
-            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 2), Kg = 2 });
-            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 3), Kg = 3 });
-            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 4), Kg = 4 });
-            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 5), Kg = 5 });
-            _fakeLocalContext.SaveChanges();
+//            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 1), Kg = 1 });
+//            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 2), Kg = 2 });
+//            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 3), Kg = 3 });
+//            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 4), Kg = 4 });
+//            _fakeLocalContext.Add(new Weight { DateTime = new DateTime(2018, 6, 5), Kg = 5 });
+//            _fakeLocalContext.SaveChanges();
 
-            var weights = _controller.Get();
+//            var weights = _controller.Get();
 
-            Assert.Equal(5, weights.Count());
-            Assert.Equal(1, weights.First(x => x.DateTime == new DateTime(2018, 6, 1)).Kg);
-            Assert.Equal(2, weights.First(x => x.DateTime == new DateTime(2018, 6, 2)).Kg);
-            Assert.Equal(3, weights.First(x => x.DateTime == new DateTime(2018, 6, 3)).Kg);
-            Assert.Equal(4, weights.First(x => x.DateTime == new DateTime(2018, 6, 4)).Kg);
-            Assert.Equal(5, weights.First(x => x.DateTime == new DateTime(2018, 6, 5)).Kg);
-        }
+//            Assert.Equal(5, weights.Count());
+//            Assert.Equal(1, weights.First(x => x.DateTime == new DateTime(2018, 6, 1)).Kg);
+//            Assert.Equal(2, weights.First(x => x.DateTime == new DateTime(2018, 6, 2)).Kg);
+//            Assert.Equal(3, weights.First(x => x.DateTime == new DateTime(2018, 6, 3)).Kg);
+//            Assert.Equal(4, weights.First(x => x.DateTime == new DateTime(2018, 6, 4)).Kg);
+//            Assert.Equal(5, weights.First(x => x.DateTime == new DateTime(2018, 6, 5)).Kg);
+//        }
 
-    }
-}
+//    }
+//}
--- src/HealthAPI/Controllers/OData/WeightsController.cs
+++ src/HealthAPI/Controllers/OData/WeightsController.cs
@@ -1,40 +1,40 @@
-﻿using System;
-using System.Linq;
-using Microsoft.AspNet.OData;
-using Microsoft.AspNetCore.Mvc;
-using Repositories;
-using Repositories.Models;
-
-namespace HealthAPI.Controllers.OData
-{
-
-    [Produces("application/json")]
-    public class WeightsController : ODataController
-    {
-        private readonly HealthContext _context;
-        
-        public WeightsController(HealthContext context)
-        {
-            _context = context;            
-        }
-
-        //   odata/Weights
-        [HttpGet]
-        [EnableQuery(AllowedQueryOptions = Microsoft.AspNet.OData.Query.AllowedQueryOptions.All)]
-        public IQueryable<Weight> Get()
-        {
-            return _context.Weights.AsQueryable();
-        }
-        
-        [HttpGet]
-        [Route("api/Weights/HealthyWeight")]
-        public Decimal GetHealthyWeight()
-        {
-            decimal healthyWeight = 88.7M;
-            
-            return healthyWeight;
-        }
-
-
-    }
-}
+﻿//using System;
+//using System.Linq;
+//using Microsoft.AspNet.OData;
+//using Microsoft.AspNetCore.Mvc;
+//using Repositories;
+//using Repositories.Models;
+//
+//namespace HealthAPI.Controllers.OData
+//{
+//
+//    [Produces("application/json")]
+//    public class WeightsController : ODataController
+//    {
+//        private readonly HealthContext _context;
+//        
+//        public WeightsController(HealthContext context)
+//        {
+//            _context = context;            
+//        }
+//
+//        //   odata/Weights
+//        [HttpGet]
+//        [EnableQuery(AllowedQueryOptions = Microsoft.AspNet.OData.Query.AllowedQueryOptions.All)]
+//        public IQueryable<Weight> Get()
+//        {
+//            return _context.Weights.AsQueryable();
+//        }
+//        
+//        [HttpGet]
+//        [Route("api/Weights/HealthyWeight")]
+//        public Decimal GetHealthyWeight()
+//        {
+//            decimal healthyWeight = 88.7M;
+//            
+//            return healthyWeight;
+//        }
+//
+//
+//    }
+//}
