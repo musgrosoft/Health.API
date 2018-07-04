29acb8e855a3d63b475f0d097139e945d607a9dd
Tim <t>
2018-07-04 18:44:05 +0100

    h

--- src/HealthAPI.Unit.Tests/Controllers/OData/BloodPressuresControllerTests.cs
+++ src/HealthAPI.Unit.Tests/Controllers/OData/BloodPressuresControllerTests.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Linq;
-using HealthAPI.Controllers.OData;
-using Repositories.Models;
-using Xunit;
+﻿//using System;
+//using System.Linq;
+//using HealthAPI.Controllers.OData;
+//using Repositories.Models;
+//using Xunit;
 
-namespace HealthAPI.Unit.Tests.Controllers.OData
-{
-    public class BloodPressuresControllerTests
-    {
-        private BloodPressuresController _controller;
-        private FakeLocalContext _fakeLocalContext;
+//namespace HealthAPI.Unit.Tests.Controllers.OData
+//{
+//    public class BloodPressuresControllerTests
+//    {
+//        private BloodPressuresController _controller;
+//        private FakeLocalContext _fakeLocalContext;
 
-        public BloodPressuresControllerTests()
-        {
-            _fakeLocalContext = new FakeLocalContext();
-            _controller = new BloodPressuresController(_fakeLocalContext);
-        }
+//        public BloodPressuresControllerTests()
+//        {
+//            _fakeLocalContext = new FakeLocalContext();
+//            _controller = new BloodPressuresController(_fakeLocalContext);
+//        }
 
-        public void Dispose()
-        {
-            _fakeLocalContext.Database.EnsureDeleted();
-        }
+//        public void Dispose()
+//        {
+//            _fakeLocalContext.Database.EnsureDeleted();
+//        }
 
-        [Fact]
-        public void ShouldGetBloodPressures()
-        {
+//        [Fact]
+//        public void ShouldGetBloodPressures()
+//        {
 
-            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 1), Systolic = 1 });
-            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 2), Systolic = 2 });
-            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 3), Systolic = 3 });
-            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 4), Systolic = 4 });
-            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 5), Systolic = 5 });
-            _fakeLocalContext.SaveChanges();
+//            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 1), Systolic = 1 });
+//            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 2), Systolic = 2 });
+//            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 3), Systolic = 3 });
+//            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 4), Systolic = 4 });
+//            _fakeLocalContext.Add(new BloodPressure { CreatedDate = new DateTime(2018, 6, 5), Systolic = 5 });
+//            _fakeLocalContext.SaveChanges();
 
-            var bloodPressures = _controller.Get();
+//            var bloodPressures = _controller.Get();
 
-            Assert.Equal(5, bloodPressures.Count());
-            Assert.Equal(1, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 1)).Systolic);
-            Assert.Equal(2, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 2)).Systolic);
-            Assert.Equal(3, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 3)).Systolic);
-            Assert.Equal(4, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 4)).Systolic);
-            Assert.Equal(5, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 5)).Systolic);
-        }
-    }
-}
+//            Assert.Equal(5, bloodPressures.Count());
+//            Assert.Equal(1, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 1)).Systolic);
+//            Assert.Equal(2, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 2)).Systolic);
+//            Assert.Equal(3, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 3)).Systolic);
+//            Assert.Equal(4, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 4)).Systolic);
+//            Assert.Equal(5, bloodPressures.First(x => x.CreatedDate == new DateTime(2018, 6, 5)).Systolic);
+//        }
+//    }
+//}
