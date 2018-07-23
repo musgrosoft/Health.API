f18399e4798c3d812e52395e71e727e88c0a368f
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-07-23 16:32:55 +0100

    fb

---
+++ src/HealthAPI/Controllers/Subscription/FitbitSubscriptionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAPI.Controllers.Subscription
+{
+    [Produces("application/json")]
+    [Route("api/FitbitSubscription")]
+    public class FitbitSubscriptionController : Controller
+    {
+        [HttpPost]
+        public IActionResult Notify()
+        {
+            return (NoContent());
+        }
+    }
+}
