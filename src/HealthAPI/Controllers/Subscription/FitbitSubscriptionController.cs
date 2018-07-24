b1391c93125b035640a3e2e0761af78641c680fb
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-07-24 16:44:26 +0100

    f

--- src/HealthAPI/Controllers/Subscription/FitbitSubscriptionController.cs
+++ src/HealthAPI/Controllers/Subscription/FitbitSubscriptionController.cs
@@ -47,7 +47,7 @@ namespace HealthAPI.Controllers.Subscription
         }
 
         [HttpGet]
-        [Route("/Subscribe")]
+        [Route("Subscribe")]
         public IActionResult Subscribe(string verify)
         {
             _fitbitClient.Subscribe();
