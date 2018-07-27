8c765ffcd655dabd256fc8d1ca31aa5b8af673f9
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-07-27 15:15:49 +0100

    h

--- src/Services/Nokia/NokiaClient.cs
+++ src/Services/Nokia/NokiaClient.cs
@@ -35,6 +35,7 @@ namespace Services.Nokia
         public async Task Subscribe()
         {
             var accessToken = _nokiaAuthenticator.GetAccessToken();
+            _logger.Log($"GetWeights accesstoken is ::: {accessToken}");
             var callback = "http://musgrosoft-health-api.azurewebsites.net/api/nokia";
 
             _httpClient.DefaultRequestHeaders.Clear();
@@ -68,6 +69,8 @@ namespace Services.Nokia
         {
             var accessToken = _nokiaAuthenticator.GetAccessToken();
 
+            _logger.Log($"GetWeights accesstoken is ::: {accessToken}");
+
             //return null;
             _httpClient.DefaultRequestHeaders.Clear();
             _httpClient.DefaultRequestHeaders.Accept.Clear();
@@ -77,6 +80,8 @@ namespace Services.Nokia
             //var response = await _httpClient.GetAsync($"{NOKIA_BASE_URL}/measure?action=getmeas&oauth_consumer_key=ebb1cbd42bb69687cb85ccb20919b0ff006208b79c387059123344b921837d8d&oauth_nonce=742bef6a3da52fbf004573d18b8f04cf&oauth_signature=cgO95H%2Fg2qx0VQ9ma2k8qeHronM%3D&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1503326610&oauth_token=7f027003b78369d415bd0ee8e91fdd43408896616108b72b97fd7c153685f&oauth_version=1.0&userid=8792669");
             var response = await _httpClient.GetAsync($"{NOKIA_BASE_URL}/measure?action=getmeas&access_token={accessToken}");
             var content = await response.Content.ReadAsStringAsync();
+
+            _logger.Log($"GetWeights ::: {response.StatusCode} , {content}");
 
             if (response.IsSuccessStatusCode)
             {
