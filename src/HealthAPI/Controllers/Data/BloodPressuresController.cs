2614d96171adc703cdf273edeca8f0c2246aed70
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-10-05 16:41:59 +0100

    bp

--- src/HealthAPI/Controllers/Data/BloodPressuresController.cs
+++ src/HealthAPI/Controllers/Data/BloodPressuresController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Elasticsearch.Net;
+using Microsoft.AspNetCore.Mvc;
 using Services.Health;
 
 namespace HealthAPI.Controllers.Data
@@ -19,6 +21,48 @@ namespace HealthAPI.Controllers.Data
         public IActionResult Get()
         {
             return Json(_healthService.GetAllBloodPressures());
+        }
+
+        [HttpGet]
+        [Route("Migrate")]
+        public IActionResult Migrate()
+        {
+            var bloodPressures = _healthService.GetAllBloodPressures();
+
+            var elasticSearchUrl = Environment.GetEnvironmentVariable("ElasticSearchUrl");
+            var node = new Uri(elasticSearchUrl);
+            var config = new ConnectionConfiguration(node);
+            var elasticSearchClient = new ElasticLowLevelClient(config);
+
+            //var elasticsearchResponse = await _elasticSearchClient
+            //    .CreatePostAsync<BytesResponse>(elasticSearchIndex,
+            //        ElasticsearchIndexType,
+            //        Guid.NewGuid().ToString(), PostData.Serializable(data));
+
+            //elasticSearchClient.IndicesDelete<BytesResponse>("bloodpressures");
+
+            foreach (var bloodPressure in bloodPressures)
+            {
+                var elasticsearchResponse = elasticSearchClient
+                    .Index<BytesResponse>(
+                        "bloodpressures",
+                        "bloodpressure",
+                        bloodPressure.CreatedDate.ToString(),
+                        PostData.Serializable(bloodPressure),
+                        null
+                    );
+            }
+
+
+
+            //if (!elasticsearchResponse.Success)
+            //    _exceptionlessClientWrapper.SubmitException(new Exception(
+            //        $"Log to elastic search failed to {ConfigurationAdapter.ElasticSearchUrl}",
+            //        elasticsearchResponse.OriginalException));
+
+
+
+            return Ok();
         }
     }
 }
