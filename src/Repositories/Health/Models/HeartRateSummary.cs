8adbcaf23723a26a887dbe964fdbdf80b0103694
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-11-08 15:15:19 +0000

    models

--- src/Repositories/Health/Models/HeartRateSummary.cs
+++ src/Repositories/Health/Models/HeartRateSummary.cs
@@ -8,10 +8,14 @@ namespace Repositories.Models
     {
         [Key]
         public DateTime CreatedDate { get; set; }
+        [Key]
+        public string Source { get; set; }
+
         public int? OutOfRangeMinutes { get; set; }
         public int? FatBurnMinutes { get; set; }
         public int? CardioMinutes { get; set; }
         public int? PeakMinutes { get; set; }
         public int? TargetCardioAndAbove { get; set; }
+        
     }
 }
