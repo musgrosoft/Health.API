6428d263d87ebfddbb38892763cbc0b626c6f525
Tim <t>
2018-11-15 21:08:15 +0000

    tab

--- src/Repositories/Health/Models/HeartRate.cs
+++ src/Repositories/Health/Models/HeartRate.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Repositories.Health.Models
 {
     public class HeartRate
     {
         public DateTime CreatedDate { get; set; }
+        [MaxLength(20)]
         public string Source { get; set; }
         public int Bpm { get; set; }
     }
