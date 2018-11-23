8a62334f2b4faab98e761e99eb65db81ced84567
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2018-11-23 16:53:18 +0000

    vw

--- src/Sql Scripts/vw_WeeklyCardio.sql
+++ src/Sql Scripts/vw_WeeklyCardio.sql
@@ -2,8 +2,7 @@ CREATE VIEW vw_WeeklyCardio AS
 
 SELECT
   DATEADD(dd,-(DATEPART(dw, CreatedDate)-1), CreatedDate) as CreatedDate,
-  SUM(CardioMinutes) as CardioMinutes,
-  SUM(PeakMinutes) as PeakMinutes
+  SUM(Cardio) as CardioAndAboveMinutes
 FROM
-  vw_DailyHeartRateSummaries
+  vw_DailyCardio
 GROUP BY DATEADD(dd,-(DATEPART(dw, CreatedDate)-1), CreatedDate)
