157b763f751de29e0030eb22628d0e8cb9c6bbc7
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-02-21 16:56:24 +0000

    views

---
+++ src/Sql Scripts/vw_MonthlyExercises.sql
@@ -0,0 +1,9 @@
+CREATE VIEW vw_MonthlyExercises AS
+
+SELECT
+  DATEADD(m, DATEDIFF(m, 0, CreatedDate), 0) as CreatedDate,
+  SUM(Metres) as Metres,
+  SUM(TotalSeconds) as TotalSeconds
+FROM
+  Exercises
+GROUP BY DATEADD(m, DATEDIFF(m, 0, CreatedDate), 0)
--- src/Sql Scripts/vw_WeeklyExercises.sql
+++ src/Sql Scripts/vw_WeeklyExercises.sql
@@ -3,7 +3,7 @@ CREATE VIEW vw_WeeklyExercises AS
 SELECT
   DATEADD(dd,-(DATEPART(dw, CreatedDate)-1), CreatedDate) as CreatedDate,
   SUM(Metres) as Metres,
-  SUM(DATEDIFF(MINUTE, '0:00:00', Time)) as Time
+  SUM(TotalSeconds) as TotalSeconds
 FROM
   Exercises
 GROUP BY DATEADD(dd,-(DATEPART(dw, CreatedDate)-1), CreatedDate)
