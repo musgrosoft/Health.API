5a896dc31bc9f2258b3fc86a35dced93eef1d3d1
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-04-29 16:59:10 +0100

    t

--- src/Importer.Fitbit.Tests/FitbitAuthenticatorTests.cs
+++ src/Importer.Fitbit.Tests/FitbitAuthenticatorTests.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Importer.Fitbit.Domain;
 using Moq;
-using Moq.Protected;
 using Services.OAuth;
 using Xunit;
 
@@ -51,46 +46,31 @@ namespace Importer.Fitbit.Tests
         }
 
 
-        //[Fact]
-        //public async Task ShouldGetAccessToken()
-        //{
-        //    //Given
-        //    _tokenService.Setup(x => x.GetFitbitRefreshToken()).Returns(Task.FromResult("abc123"));
+        [Fact]
+        public async Task ShouldGetAccessToken()
+        {
+            //Given
+            _tokenService.Setup(x => x.GetFitbitRefreshToken()).Returns(Task.FromResult("abc123"));
 
-        //    HttpRequestMessage capturedRequest = new HttpRequestMessage();
-        //    _httpMessageHandler.Protected().Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
-        //        .Returns(Task.FromResult(new HttpResponseMessage
-        //        {
-        //            StatusCode = HttpStatusCode.OK,
-        //            Content = new StringContent(@"{
-        //            ""access_token"": ""aaa2"",
-        //            ""expires_in"": 3600,
-        //            ""refresh_token"": ""bbb2"",
-        //            ""scope"": ""xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"",
-        //            ""token_type"": ""Bearer"",
-        //            ""user_id"": ""26FWFL""
-        //        }")
-        //        })).Callback<HttpRequestMessage, CancellationToken>((h, c) => capturedRequest = h);
+            _fitbitClient.Setup(x => x.GetTokensWithRefreshToken("abc123"))
+                .Returns(Task.FromResult(new FitbitRefreshTokenResponse
+                {
+                    access_token = "aaa2",
+                    refresh_token = "bbb2"
+                }));
 
 
-        ////When
-        //await _fitbitAuthenticator.GetAccessToken();
+            //When
+            await _fitbitAuthenticator.GetAccessToken();
 
-        //    //Then
+            //Then
 
-        //    Assert.Equal("https://api.fitbit.com/oauth2/token", capturedRequest.RequestUri.AbsoluteUri);
-        //    Assert.True(capturedRequest.Headers.Contains("Authorization"));
-        //    Assert.Equal(new List<string> { "Basic " + Base64Encode("123456:secret") }, capturedRequest.Headers.GetValues("Authorization"));
 
-        //    Assert.Equal(HttpMethod.Post, capturedRequest.Method);
 
-        //    Assert.Contains("grant_type=refresh_token", (await capturedRequest.Content.ReadAsStringAsync()));
-        //    Assert.Contains("refresh_token=abc123", (await capturedRequest.Content.ReadAsStringAsync()));
+            _tokenService.Verify(x => x.SaveFitbitAccessToken("aaa2"));
+            _tokenService.Verify(x => x.SaveFitbitRefreshToken("bbb2"));
 
-        //    _tokenService.Verify(x => x.SaveFitbitAccessToken("aaa2"));
-        //    _tokenService.Verify(x => x.SaveFitbitRefreshToken("bbb2"));
-
-        //}
+        }
 
 
     }
