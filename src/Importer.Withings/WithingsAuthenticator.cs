ac95fecf6d2ae8817e9a4f9cda5209ac2fc6531e
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-05-10 16:42:19 +0100

    logging

--- src/Importer.Withings/WithingsAuthenticator.cs
+++ src/Importer.Withings/WithingsAuthenticator.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Services.OAuth;
+using Utils;
 
 namespace Importer.Withings
 {
@@ -8,17 +9,22 @@ namespace Importer.Withings
         private readonly ITokenService _oAuthService;
 
         private readonly IWithingsClient _withingsClient;
+        private readonly ILogger _logger;
 
 
-        public WithingsAuthenticator(ITokenService oAuthService, IWithingsClient withingsClient)
+        public WithingsAuthenticator(ITokenService oAuthService, IWithingsClient withingsClient, ILogger logger)
         {
             _oAuthService = oAuthService;
             _withingsClient = withingsClient;
+            _logger = logger;
         }
 
         public async Task SetTokens(string authorizationCode)
         {
+            await _logger.LogMessageAsync("the authorisation code is " + authorizationCode);
             var tokenResponse = await _withingsClient.GetTokensByAuthorisationCode(authorizationCode);
+
+            await _logger.LogMessageAsync("the resopnse is at : " + tokenResponse.access_token + " and rt : " + tokenResponse.refresh_token);
 
             var tokenResponseAccessToken = tokenResponse.access_token;
             var tokenResponseRefreshToken = tokenResponse.refresh_token;
