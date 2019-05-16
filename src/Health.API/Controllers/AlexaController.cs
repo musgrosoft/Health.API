e794972a793dc3aec4ea9395b69737e593db4776
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-05-16 16:28:59 +0100

    flash briefing

---
+++ src/Health.API/Controllers/AlexaController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthAPI.Controllers
+{
+    public class AlexaController : Controller
+    {
+        // GET
+        public IActionResult FlashBriefing()
+        {
+            var flashBriefings = new List<FlashBriiefing>
+                {
+                new FlashBriiefing
+                {
+                    uid = "EXAMPLE_CHANNEL_MULTI_ITEM_JSON_TTS_1",
+                    updateDate = new DateTime(2016,4,10),
+                    titleText = "Multi Item JSON (TTS)",
+                    mainText = "This channel has multiple TTS JSON items. This is the first item.",
+                    redirectionUrl = "https://www.amazon.com"
+
+                },
+                new FlashBriiefing
+                {
+                    uid = "EXAMPLE_CHANNEL_MULTI_ITEM_JSON_TTS_2",
+                    updateDate = new DateTime(2016,4,10),
+                    titleText = "Multi Item JSON (TTS)",
+                    mainText = "This channel has multiple TTS JSON items. This is the second item.",
+                    redirectionUrl = "https://www.amazon.com"
+
+                }
+
+                };
+
+
+            //Content-Type: application/json
+            return Ok(flashBriefings);
+        }
+    }
+}
---
+++ src/Health.API/Controllers/FlashBriiefing.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HealthAPI.Controllers
+{
+    public class FlashBriiefing
+    {
+        public string uid { get; set; }
+        public DateTime updateDate { get; set; }
+        public string titleText { get; set; }
+        public string mainText { get; set; }
+        public string redirectionUrl { get; set; }
+
+
+    }
+}
