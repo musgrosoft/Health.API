40ac680afe3ce34a2339ff17dc1be56f37465566
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-06-14 15:15:43 +0100

    update flash briefings

--- src/Health.API/Controllers/AlexaController.cs
+++ src/Health.API/Controllers/AlexaController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Services.Health;
+using Services.OAuth;
 
 namespace HealthAPI.Controllers
 {
@@ -11,10 +13,12 @@ namespace HealthAPI.Controllers
     public class AlexaController : Controller
     {
         private readonly IHealthService _healthService;
+        private readonly ITokenService _tokenService;
 
-        public AlexaController(IHealthService healthService)
+        public AlexaController(IHealthService healthService, ITokenService tokenService)
         {
             _healthService = healthService;
+            _tokenService = tokenService;
         }
 
         private string PrettifyDaysOld(double daysOld)
@@ -38,7 +42,7 @@ namespace HealthAPI.Controllers
         // GET
         [HttpGet]
         [Route("FlashBriefing")]
-        public IActionResult FlashBriefing()
+        public async Task<IActionResult> FlashBriefing()
         {
             var latestWeightDate = _healthService.GetLatestWeightDate(DateTime.MinValue);
             var daysOldWeight = DaysOld(latestWeightDate);
@@ -64,48 +68,140 @@ namespace HealthAPI.Controllers
             var latestBloodpressures = _healthService.GetLatestBloodPressures();
             var latestRestingHeartRates = _healthService.GetLatestRestingHeartRates();
 
+            var flashBriefings = new List<FlashBriefing>();
 
+            var technicalErrorFlashBriefing = await GetTechnicalErrorFlashBriefing();
 
-            var flashBriefings = new List<FlashBriefing>
-                {
+            if (technicalErrorFlashBriefing != null)
+            {
+                flashBriefings.Add(technicalErrorFlashBriefing);
+            }
 
-                    new FlashBriefing
-                    {
-                        uid = "1_AGE_OF_DATA",
-                        updateDate = DateTime.Now.AddMinutes(1).ToString("yyyy-MM-ddTHH:mm:ss.0Z"), //"2019-06-10T22:34:51.0Z" ,
-                        titleText = "Latest recordings",
-                        mainText = $"Latest Weight recorded {daysOldWeightExpression}. " +
-                                   $"Latest Blood pressure recorded {daysOldBloodpressureExpression}. " +
-                                   $"Latest Resting Heart Rate recorded {daysOldRestingHeartRateExpression}. " +
-                                   $"Latest Drink recorded {daysOldDrinkDateExpression}. " +
-                                   $"Latest Exercise recorded {daysOldExerciseDateExpression}. ",
-                        redirectionUrl = "https://www.amazon.com"
-                    },
+            var oldDataFlashBriefing = GetOldDataFlashBriefing();
 
-                new FlashBriefing
-                {
-                    uid = "2_DATA",
-                    updateDate = DateTime.Now.AddMinutes(2).ToString("yyyy-MM-ddTHH:mm:ss.0Z"), //"2019-06-10T22:34:51.0Z" ,
-                    titleText = "Data",
-                    mainText = $"Your Weight is {latestWeights.Average(x=>x.Kg).Value:0.#} kg. " +
-                               $"Your blood pressure is {latestBloodpressures.Average(x=>x.Systolic).Value:N0} over {latestBloodpressures.Average(x=>x.Diastolic).Value:N0}. " +
-                               $"Your resting heart rate is {latestRestingHeartRates.Average(x=>x.Beats):N0}. ",
-                    redirectionUrl = "https://www.amazon.com"
-                },
-//                new FlashBriefing
-//                {
-//                    uid = "3_TARGETS",
-//                    updateDate = DateTime.Now.AddMinutes(3).ToString("yyyy-MM-ddTHH:mm:ss.0Z"), //"2019-06-10T22:34:51.0Z" ,
-//                    titleText = "Targets",
-//                    mainText = $"This is some unique content. ",
-//                    redirectionUrl = "https://www.amazon.com"
-//                }
+            if (oldDataFlashBriefing != null)
+            {
+                flashBriefings.Add(oldDataFlashBriefing);
+            }
 
-                };
+//            var hitTargetsFlashBriefing = GetHitTargetsFlashBriefing();
 
 
             //Content-Type: application/json
             return Ok(flashBriefings);
+        }
+
+//        private FlashBriefing GetHitTargetsFlashBriefing()
+//        {
+//            var latestWeights = _healthService.GetLatestWeights();
+//            var latestBloodpressures = _healthService.GetLatestBloodPressures();
+//            var latestRestingHeartRates = _healthService.GetLatestRestingHeartRates();
+//
+//            var averageWeight = latestWeights.Average(x => x.Kg);
+//            var targetWeight 
+//        }
+
+        private FlashBriefing GetOldDataFlashBriefing()
+        {
+            var latestWeightDate = _healthService.GetLatestWeightDate(DateTime.MinValue);
+            var daysOldWeight = DaysOld(latestWeightDate);
+            var daysOldWeightExpression = PrettifyDaysOld(daysOldWeight);
+
+            var latestBloodPressureDate = _healthService.GetLatestBloodPressureDate(DateTime.MinValue);
+            var daysOldBloodpressure = DaysOld(latestBloodPressureDate);
+            var daysOldBloodpressureExpression = PrettifyDaysOld(daysOldBloodpressure);
+
+            var latestRestingHeartRate = _healthService.GetLatestRestingHeartRateDate(DateTime.MinValue);
+            var daysOldRestingHeartRate = DaysOld(latestRestingHeartRate);
+            var daysOldRestingHeartRateExpression = PrettifyDaysOld(daysOldRestingHeartRate);
+
+            var latestDrinkDate = _healthService.GetLatestDrinkDate();
+            var daysOldDrinkDate = DaysOld(latestDrinkDate);
+            var daysOldDrinkDateExpression = PrettifyDaysOld(daysOldDrinkDate);
+
+            var latestExerciseDate = _healthService.GetLatestExerciseDate(DateTime.MinValue);
+            var daysOldExerciseDate = DaysOld(latestExerciseDate);
+            var daysOldExerciseDateExpression = PrettifyDaysOld(daysOldExerciseDate);
+            
+            var messages = "";
+
+            if (daysOldWeight > 4)
+            {
+                messages += $"Weight was last updated {daysOldWeightExpression}. ";
+            }
+
+            if (daysOldBloodpressure > 4)
+            {
+                messages += $"Blood pressure was last updated {daysOldBloodpressureExpression}. ";
+            }
+
+            if (daysOldRestingHeartRate > 2)
+            {
+                messages += $"Resting heart rate was last updated {daysOldRestingHeartRateExpression}. ";
+            }
+
+            if (daysOldDrinkDate > 3)
+            {
+                messages += $"Drinks were last updated {daysOldDrinkDateExpression}. ";
+            }
+
+            if (daysOldExerciseDate > 3)
+            {
+                messages += $"Exercise was last updated {daysOldExerciseDateExpression}. ";
+            }
+
+            if (string.IsNullOrWhiteSpace(messages))
+            {
+                return null;
+            }
+            else
+            {
+                return new FlashBriefing
+                {
+                    uid = "OLD DATA",
+                    updateDate = DateTime.Now.AddMinutes(2).ToString("yyyy-MM-ddTHH:mm:ss.0Z"),
+                    titleText = "Old Date",
+                    mainText = $"There is some old data. {messages}",
+                    redirectionUrl = "https://www.amazon.com"
+                };
+            }
+        }
+
+        private async Task<FlashBriefing> GetTechnicalErrorFlashBriefing()
+        {
+            var fitbitRefreshToken = await _tokenService.GetFitbitRefreshToken();
+            var withingsRefreshToken = await _tokenService.GetWithingsRefreshToken();
+
+            var messages = "";
+
+            if (string.IsNullOrWhiteSpace(fitbitRefreshToken))
+            {
+                messages += "Fitbit Refresh Token is empty.";
+            }
+
+            if (string.IsNullOrWhiteSpace(withingsRefreshToken))
+            {
+                messages += "Withings Refresh Token is empty.";
+            }
+
+
+            if (string.IsNullOrWhiteSpace(messages))
+            {
+                return null;
+            }
+            else
+            {
+                return new FlashBriefing
+                {
+                    uid = "TECHNICAL ERRORS",
+                    updateDate = DateTime.Now.AddMinutes(1).ToString("yyyy-MM-ddTHH:mm:ss.0Z"), 
+                    titleText = "Technical Errors",
+                    mainText = $"There are technical errors. {messages}",
+                    redirectionUrl = "https://www.amazon.com"
+                };
+            }
+
+
         }
     }
 }
