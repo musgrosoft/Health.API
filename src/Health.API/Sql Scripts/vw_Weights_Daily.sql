aed981ca63262a19d675da27a3597f0b943364ad
Tim <4159866+musgrosoft@users.noreply.github.com>
2019-07-10 14:53:41 +0100

    Update vw_Weights_Daily.sql

--- src/Health.API/Sql Scripts/vw_Weights_Daily.sql
+++ src/Health.API/Sql Scripts/vw_Weights_Daily.sql
@@ -1,15 +1,15 @@
 CREATE VIEW vw_Weights_Daily AS
 
 SELECT 
-    CalendarDate, 
+    Date, 
     Kg, 
 	FatRatioPercentage, 
-    AVG(Kg) OVER (ORDER BY CalendarDate ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS MovingAverageKg,
-	AVG(FatRatioPercentage) OVER (ORDER BY CalendarDate ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS MovingAverageFatRatioPercentage,
+    AVG(Kg) OVER (ORDER BY Date ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS MovingAverageKg,
+	AVG(FatRatioPercentage) OVER (ORDER BY Date ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS MovingAverageFatRatioPercentage,
 	CASE 
-        WHEN CalendarDate >= '2019/01/01' THEN 86    - ((3.000/365) * (DATEDIFF(day , '2019/01/01' , CalendarDate)))
-		WHEN CalendarDate >= '2018/09/01' THEN 88.7  - ((0.250/30)  * (DATEDIFF(day , '2018/09/01' , CalendarDate)))
-		WHEN CalendarDate >= '2018/05/01' THEN 90.74 - ((0.500/30)  * (DATEDIFF(day , '2018/05/01' , CalendarDate)))
+        WHEN Date >= '2019/01/01' THEN 86    - ((3.000/365) * (DATEDIFF(day , '2019/01/01' , Date)))
+		WHEN Date >= '2018/09/01' THEN 88.7  - ((0.250/30)  * (DATEDIFF(day , '2018/09/01' , Date)))
+		WHEN Date >= '2018/05/01' THEN 90.74 - ((0.500/30)  * (DATEDIFF(day , '2018/05/01' , Date)))
         ELSE NULL
     END AS TargetKg
 FROM
@@ -23,4 +23,4 @@ FROM
 		GROUP BY CAST(CreatedDate AS DATE)
 	) AS AverageDailyWeights 
 		RIGHT JOIN dbo.CalendarDates Cal
-		ON Cal.CalendarDate = AverageDailyWeights.CreatedDate
+		ON Cal.Date = AverageDailyWeights.CreatedDate
