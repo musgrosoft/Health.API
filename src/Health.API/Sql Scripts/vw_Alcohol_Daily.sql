c43167dc1251210c209bab1656feeef473973e0c
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-07-11 16:53:20 +0100

    views

--- src/Health.API/Sql Scripts/vw_Alcohol_Daily.sql
+++ src/Health.API/Sql Scripts/vw_Alcohol_Daily.sql
@@ -1,23 +1,23 @@
 CREATE VIEW vw_DailyAlcoholIntakes AS
 
 SELECT
-    CalendarDate,
+    Date,
     Units,
 	TargetUnits,
-	SUM(TargetUnits) OVER (ORDER BY CalendarDate) + 5147.7 as CumSumTargetUnits,	
-	SUM(Units) OVER (ORDER BY CalendarDate) as CumSumUnits
+	SUM(TargetUnits) OVER (ORDER BY Date) + 5147.7 as CumSumTargetUnits,	
+	SUM(Units) OVER (ORDER BY Date) as CumSumUnits
 FROM
 (
 	SELECT 
-		CalendarDate,
+		Date,
 		Units,
 		CASE 
-			WHEN CalendarDate > '2018/05/29' THEN 4
+			WHEN Date > '2018/05/29' THEN 4
 			ELSE NULL
 		END AS TargetUnits
 	FROM 
 		Drinks D
-		INNER JOIN Calendar C
-		ON D.CreatedDate = C.CalendarDate
+		INNER JOIN CalendarDates C
+		ON D.CreatedDate = C.Date
 		 
 ) AS DailyAlcohol
--- src/Health.API/Sql Scripts/vw_Exercises_Daily.sql
+++ src/Health.API/Sql Scripts/vw_Exercises_Daily.sql
@@ -1,12 +1,12 @@
 CREATE VIEW vw_DailyExercises AS
 
 SELECT 
-	CalendarDate AS CreatedDate, 
+	Date AS CreatedDate, 
 	SUM(COALESCE(Metres,0)) AS Metres,
 	SUM(COALESCE(TotalSeconds,0)) AS TotalSeconds
 FROM 
-	Calendar C
+	CalendarDates C
 LEFT OUTER JOIN
 	Exercises E
-ON	C.CalendarDate = E.CreatedDate
-GROUP BY CalendarDate
+ON	C.Date = E.CreatedDate
+GROUP BY Date
