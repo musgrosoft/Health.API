43e019ee49a17f0f5e6d63e368b9439410a62291
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2019-09-05 16:49:32 +0100

    v

--- src/Health.API/Sql Scripts/vw_Fitbit_Sleeps_Daily.sql
+++ src/Health.API/Sql Scripts/vw_Fitbit_Sleeps_Daily.sql
@@ -2,14 +2,14 @@ CREATE VIEW vw_FitbitSleeps_Daily AS
 
 
 		SELECT 
-		CAST(StartTime AS DATE) AS StartDate,		
+		CAST(DATEADD(hour,-12,StartTime) AS DATE) AS CorrectedStartDate,		
 		SUM(LightMinutes)/60 AS LightMinutes,
 		SUM(DeepMinutes)/60 AS DeepMinutes,
 		SUM(RemMinutes)/60 AS RemMinutes,
 		(SUM(LightMinutes) + SUM(DeepMinutes) + SUM(RemMinutes))  AS TotalSleepMinutes,
 		SUM (MinutesAsleep) AS MinutesAsleep,
 		--MAX(StartTime) AS StartTime,
-		MAX(CAST(EndTime AS DATE)) AS EndDate
+		--MAX(CAST(EndTime AS DATE)) AS EndDate
 		FROM
 		dbo.MyFitbitSleeps
-		GROUP BY CAST(StartTime AS DATE)
+		GROUP BY CAST(DATEADD(hour,-12,StartTime) AS DATE)
