8c379387076351cea62f7a769310b2ca46309779
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2020-01-17 14:56:10 +0000

    start in memory integration/aceeptance tests

---
+++ Health.API.Integration.Tests/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace Health.API.Integration.Tests
+{
+    public class Class1 : IClassFixture<WebApplicationFactory<Health.API.Controllers.CanaryController>>
+    {
+        private readonly WebApplicationFactory<Health.API.Controllers.CanaryController> _factory;
+
+        public Class1(WebApplicationFactory<Health.API.Controllers.CanaryController> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task thing()
+        {
+            var client = _factory.CreateClient();
+
+            var response = await client.GetAsync("/");
+
+            //Then
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("Hello Tim", await response.Content.ReadAsStringAsync());
+        }
+    }
+}
