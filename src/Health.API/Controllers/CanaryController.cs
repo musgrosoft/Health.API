14cf44242e11ae3a54c7fd9e8697aa6d5a741a8a
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2020-01-17 15:19:59 +0000

    fitbit sleeps in canary

--- src/Health.API/Controllers/CanaryController.cs
+++ src/Health.API/Controllers/CanaryController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Fitbit;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +12,27 @@ namespace Health.API.Controllers
     [ApiController]
     public class CanaryController : ControllerBase
     {
+        private readonly IFitbitService _fitbitService;
+
+        public CanaryController(IFitbitService fitbitService)
+        {
+            _fitbitService = fitbitService;
+        }
 
         [HttpGet]
         //[Route("Notification")]
         public IActionResult Index()
         {
             return Ok("Hello Tim");
+        }
+
+        [HttpGet]
+        //[Route("Notification")]
+        public IActionResult Fitbit()
+        {
+            var sleeps = _fitbitService.GetSleepSummaries(DateTime.Now.AddDays(-10), DateTime.Now);
+
+            return Ok(sleeps);
         }
     }
 }
