77afe47c8d32aeb120886c029eae8486da58c221
Tim Musgrove <tim.musgrove@totaljobsgroup.com>
2020-01-24 11:35:12 +0000

    db controller

--- src/Health.API/Controllers/DatabaseController.cs
+++ src/Health.API/Controllers/DatabaseController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Repositories;
 
 namespace Health.API.Controllers
@@ -15,11 +18,56 @@ namespace Health.API.Controllers
         }
 
         [HttpGet]
-        public IActionResult Drop()
+        [Route("EnsureDeleted")]
+        public IActionResult EnsureDeleted()
         {
             var result = _healthContext.Database.EnsureDeleted();
 
             return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("EnsureCreated")]
+        public IActionResult EnsureCreated()
+        {
+            var result = _healthContext.Database.EnsureCreated();
+
+            return Ok(result);
+        }
+
+
+        [HttpGet]
+        [Route("RefreshViews")]
+        public IActionResult RefreshViews()
+        {
+            //Task.Run(async () => await logger.LogMessageAsync("DB WAS JUST RECREATED, START RUNNING IN VIEWS"));
+
+            _healthContext.Database.SetCommandTimeout(180);
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/Drop_All_Views.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Alcohol_Daily.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Alcohol_Monthly.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Alcohol_Weekly.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_BloodPressures_Daily.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Exercises_Daily.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Exercises_Daily_2.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Exercises_Daily_Agg.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Exercises_Monthly.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Exercises_Weekly.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Weights_Daily.sql"));
+
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Sleeps_Daily.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Sleeps_Monthly.sql"));
+            _healthContext.Database.ExecuteSqlRaw(System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "/Sql Scripts/vw_Sleeps_Weekly.sql"));
+
+            //Task.Run(async () => await logger.LogMessageAsync("FINISHED RUNNING IN VIEWS"));
+
+            return Ok("ok");
         }
     }
 }
